8f7eff1a56bcb160b1457b4d53bc3e1c2993e959
Scott Schwab <scottschwab86@gmail.com>
2024-02-04 14:10:46 -0600

    Complete challenge

--- javascript/find_shortest_string_recursive.js
+++ javascript/find_shortest_string_recursive.js
@@ -1,6 +1,36 @@
 function findShortestStringRecursive(arr) {
-  // type your code here
+  if (arr.length === 1) {
+    return arr[0]
+  }
+  if (arr.length === 2) {
+    return arr[0].length <= arr[1].length ? arr[0] : arr[1]
+  }
+  else {
+    if (arr[1].length < arr[0].length) {
+      arr.shift()
+      return findShortestStringRecursive(arr)
+    }
+
+    if (arr[0].length <= arr[1].length) {
+      arr.splice(1, 1)
+      return findShortestStringRecursive(arr)
+    }
+  }
 }
+
+let longArr = [];
+for (let j = 0; j < 100; j++) {
+  longArr.push("hello")
+}
+
+let startTime = Date.now();
+for (let i = 0; i < 1000; i++) {
+  findShortestStringRecursive(longArr)
+  findShortestStringRecursive(["a", "bb", "c"])
+}
+let endTime = Date.now()
+
+console.log("Avg time: " + (endTime - startTime) / 2000)
 
 if (require.main === module) {
   // add your own tests in here
@@ -16,6 +46,22 @@ if (require.main === module) {
 
   console.log("Expecting: 'lily'");
   console.log("=>", findShortestStringRecursive(['flower', 'juniper', 'lily', 'dandelion']));
+
+  console.log("");
+
+  console.log("Expecting: 'cat'");
+  console.log("=>", findShortestStringRecursive(['dogbaby', 'cat', 'jammy', 'hamtaro']));
+
+
+  // console.log("");
+
+  // console.log("Expecting: 'b'");
+  // console.log("=>", findShortestStringRecursive(['b']));
+
+  // console.log("");
+
+  // console.log("Expecting: ''");
+  // console.log("=>", findShortestStringRecursive([]));
 }
 
 module.exports = findShortestStringRecursive;
